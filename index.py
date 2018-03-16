6e1ea0c84ca9b717d86286f10b1c28f8130e3fe7
philip.hogan <philip.hogan@student39.org>
2018-03-16 14:03:54 -0400

    also yells at you if you include numbers in your name

--- index.py
+++ index.py
@@ -2,8 +2,12 @@ print("Hello, World!")
 
 name = input("What's your name?\n")
 
+chars = set('0123456789')
+
 if name == "":
     print("I SAID TELL ME YOUR NAME DANGIT GO AWAY IM DONE WITH YOU")
     exit(1)
+elif any((c in chars) for c in name):
+    print("What idiot has a NUMBER in their freakin name?")
 else:
     print("Oh hello there " + name + "!")
