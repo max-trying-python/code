f10907a509395f0bcc146062155dc0d7c9d50fa1
philip.hogan <philip.hogan@student39.org>
2018-03-16 14:22:12 -0400

    ask you until you get it right

--- index.py
+++ index.py
@@ -1,15 +1,21 @@
 print("Hello, World!")
 
-name = input("What's your name?\n")
+answered = False
+tries = 0
+
 
 chars = set('0123456789')
 
-if name == "":
-    print("I SAID TELL ME YOUR NAME DANGIT GO AWAY IM DONE WITH YOU")
-    exit(1)
-elif any((c in chars) for c in name):
-    print("What idiot has a NUMBER in their freakin name?")
-elif name.count(" ") < 1:
-    print("Full name plz")
-else:
-    print("Oh hello there, " + name + "!")
+while not answered:
+    name = input("What's your name?\n")
+    tries += 1
+    if name == "":
+        print("I SAID TELL ME YOUR NAME DANGIT GO AWAY IM DONE WITH YOU")
+        exit(1)
+    elif any((c in chars) for c in name):
+        print("What idiot has a NUMBER in their freakin name?")
+    elif name.count(" ") < 1:
+        print("Full name plz")
+    else:
+        print("Oh hello there, " + name + "!")
+        answered = True
