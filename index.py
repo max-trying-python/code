6eb41e5d7f54d6065e4ba7b11b2c5bfc9e1ef905
philip.hogan <philip.hogan@student39.org>
2018-03-16 13:57:06 -0400

    yells at you when you don't give your name

--- index.py
+++ index.py
@@ -2,4 +2,8 @@ print("Hello, World!")
 
 name = input("What's your name?\n")
 
-print("Oh hello there " + name + "!")
+if name == "":
+    print("I SAID TELL ME YOUR NAME DANGIT GO AWAY IM DONE WITH YOU")
+    exit(1)
+else:
+    print("Oh hello there " + name + "!")
