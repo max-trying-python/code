49073a76b7df3c124c6facbd5bdb3c446c45defb
philip.hogan <philip.hogan@student39.org>
2018-03-22 14:22:52 -0400

    starting on overtimepay

---
+++ OvertimePay.py
@@ -0,0 +1,10 @@
+hours = None
+try:
+    int(input("How many hours did you work?"))
+except Exception as e:
+    print("You're stuposed to give a number you idiot")
+    exit()
+if hours > 40:
+    pass
+else:
+    pass
