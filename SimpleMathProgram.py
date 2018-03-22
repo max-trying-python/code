5735a305fc6fbeb68aa51ba90c7776839e6b1b7c
philip.hogan <philip.hogan@student39.org>
2018-03-22 13:08:53 -0400

    keep code DRY

--- SimpleMathProgram.py
+++ SimpleMathProgram.py
@@ -1,10 +1,16 @@
+# static prices
 priceOfDozenDonuts = 4.99
 pricePerDonut = priceOfDozenDonuts / 12
 cappucino = 2.99
 
-print("Change owed for first situation: " + str(round(1 - pricePerDonut, 2)))
+
+def roundAndStr(num):
+    return str(round(num, 2))
+
+
+print("Change owed for first situation: " + roundAndStr(1 - pricePerDonut))
 input("Press enter to continue...")
 
 newPrice = round(20 - (2 * pricePerDonut) - cappucino, 2)
-print("Change owed for second situation: " + str(newPrice))
+print("Change owed for second situation: " + roundAndStr(newPrice))
 input("Press enter to exit...")
