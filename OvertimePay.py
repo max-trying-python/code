c7195143eabae68490def49736c527d6696e495b
philip.hogan <philip.hogan@student39.org>
2018-03-26 12:44:18 -0400

    OOF spelling error

--- OvertimePay.py
+++ OvertimePay.py
@@ -2,7 +2,7 @@ hours = None
 try:
     int(input("How many hours did you work?"))
 except Exception as e:
-    print("You're stuposed to give a number you idiot")
+    print("You're supposed to give a number you idiot")
     exit()
 if hours > 40:
     pass
