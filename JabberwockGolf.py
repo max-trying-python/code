57c02434c01eb32f8b9d0781644d3a614dd038ce
philip.hogan <philip.hogan@student39.org>
2018-03-27 13:31:14 -0400

    %->Jabberwock 1013 bytes

--- JabberwockGolf.py
+++ JabberwockGolf.py
@@ -1,21 +1,21 @@
-print("""Jabberwocky by Lewis Carroll
+print("""%y by Lewis Carroll
 
 Twas brillig, and the slithy toves
 |Did gyre and gimble in the wabe.
 All mimsy were the borogoves,
-|And the mome raths outgrabe._Beware the Jabberwock, my son!
+|And the mome raths outgrabe._Beware the %, my son!
 |The jaws that bite, the claws that catch!
 Beware the Jubjub bird, and shun
 |The frumious Bandersnatch!_He took his vorpal sword in hand;
 |Long time the manxome foe he sought
 So rested he by the Tumtum tree
 |And stood awhile in thought._And, as in uffish thought he stood,
-|The Jabberwock, with eyes of flame,
+|The %, with eyes of flame,
 Came whiffling through the tulgey wood,
 |And burbled as it came!_One, two! One, two! And through and through
 |The vorpal blade went snicker-snack!
 He left it dead, and with its head
-|He went galumphing back._And hast thou slain the Jabberwock?
+|He went galumphing back._And hast thou slain the %?
 |Come to my arms, my beamish boy!
 O frabjous day! Callooh! Callay!
 |He chortled in his joy._Twas brillig, and the slithy toves
@@ -24,4 +24,4 @@ All mimsy were the borogoves,
 |And the mome raths outgrabe.""".replace("_","""
 
 
-""").replace("|","      "))
+""").replace("|","      ").replace("%","Jabberwock"))
