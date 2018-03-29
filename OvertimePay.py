2af98e5753b83b790124ec1b902b65856cc52871
philip.hogan <philip.hogan@student39.org>
2018-03-29 13:20:59 -0400

    overtime pay

--- OvertimePay.py
+++ OvertimePay.py
@@ -1,10 +1,29 @@
-hours = None
+# # # calculate overtime pay
+
+# # constants
+# how much overtime is multiplied by
+overtime_multiplier = 1.5
+# money earned per hour in dollars
+money_per_hour = 10
+# the most hours you can work WITHOUT GOING INTO OVERTIME
+min_overtime_hours = 40
+
+hours_worked = None
+pay = None
 try:
-    int(input("How many hours did you work?"))
+    hours_worked = float(input("How many hours did you work? "))
 except Exception as e:
-    print("You're supposed to give a number you idiot")
+    print("You're supposed to give a number you idiot ")
     exit()
-if hours > 40:
-    pass
+if hours_worked <= min_overtime_hours:
+    msg = "No overtime pay! You didn't work for more than {} hours! ".format(min_overtime_hours)
+    print(msg)
+    pay = money_per_hour * hours_worked
+elif hours_worked == 0:
+    print("You can't work for no hours")
+    exit()
 else:
-    pass
+    pay = money_per_hour * 40 * overtime_multiplier
+    pay += money_per_hour * (hours_worked - 40)
+
+print("You earned ${}".format(pay))
