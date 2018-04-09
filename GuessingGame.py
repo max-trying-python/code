0d65535226eb91815c499a02dcb43ddf2c12e6d2
philip.hogan <philip.hogan@student39.org>
2018-04-09 13:40:34 -0400

    Guessing Game!

--- GuessingGame.py
+++ GuessingGame.py
@@ -5,5 +5,23 @@ num = random.randrange(1, 21)
 # if the user guessed the number right
 guessed = False
 
+# repeat for 5 times
 for i in range(0, 5):
     guess = int(input("Guess the number!\n> "))
+
+    # too high
+    if guess > num:
+        print("You guessed too high!")
+    # too low
+    elif guess < num:
+        print("You guessed too low!")
+    # otherwise must be just right
+    else:
+        print("YOU WIN!")
+        guessed = True
+        break
+
+
+# if the user hasn't guessed in time
+if not guessed:
+    print("\nYou used up your 5 guesses!")
