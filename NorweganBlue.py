c3567b29160dd022be264291750094d59130564a
philip.hogan <philip.hogan@student39.org>
2018-04-09 14:01:18 -0400

    create norwegan blue

---
+++ NorweganBlue.py
@@ -0,0 +1,3 @@
+# constant strings
+str1 = " no no he's not dead, he's restin'! Remarkable bird, the Norwegian Blue, idn'it, ay? Beautiful plumage! "
+str2 = " I took the liberty of examining that parrot when I got it home, and I discovered the only reason that it had been sitting on its perch in the first place was that it had been NAILED there."
