deaad776777346fd167d278fd922b18dbcbad25f
philip.hogan <booah8@gmail.com>
2018-05-09 14:14:04 -0400

    added comments and lowest card

--- GameCards.py
+++ GameCards.py
@@ -2,16 +2,26 @@ import random
 
 # make the deck
 deck = list(range(1, 14))
+
+# the amount of cards to draw
 NUM_TO_DRAW = 6
 
+# a list of all the cards to remove from the deck
 pickedCards = []
+
+# repeat forever until we have enough cards
 while len(pickedCards) < NUM_TO_DRAW:
+    # pick a card to remove from the deck
     cardToAdd = random.randint(1, len(deck))
+
+    # catch errors when trying to remove a card that already exists in the deck
     try:
         deck.remove(cardToAdd)
     except ValueError as e:
         pass
     else:
+        # add the card to our picked selectoin
         pickedCards.append(cardToAdd)
 
-print(pickedCards)
+print("Picked these cards: ", pickedCards)
+print("Lowest card: ", min(pickedCards))
