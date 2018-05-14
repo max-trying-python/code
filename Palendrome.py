27fdf5b7a1da6144d25114818807b9feccf136dd
philip.hogan <booah8@gmail.com>
2018-05-14 12:57:29 -0400

    add comments to the palendrome code

--- Palendrome.py
+++ Palendrome.py
@@ -1,1 +1,4 @@
+# this is a lambda that combines it's first argument with a reversed, first-char stripped version of itself
 palindrome = lambda word: word + word[-2::-1]
+
+print(palindrome(input()))
