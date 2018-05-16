8789eafa86195c2300cc7f6c798f09b745461cef
philip.hogan <booah8@gmail.com>
2018-05-16 13:06:31 -0400

    finish the scary word search

--- ScaryWordSearch.py
+++ ScaryWordSearch.py
@@ -1,9 +1,23 @@
 # constant words
 words = ["mummy", "werewolf", "disguise", "graveyard", "cackle", "cobweb", "dracula", "ghostly", "mischief", "panic"]
 
+# add spaces in the words
+words = [" ".join(list(word)) for word in words]
+
 # read the werewolf file
 wordLines = open("werewolf.txt").read().split("\n")
 
-newWords = [str.upper(line) for line in wordLines]
 
-print(newWords)
+# capitalize the words in a line
+def capitalizeLine(line):
+    for word in words:
+        # capitalize the word if we find it
+        line = line.replace(word, str.upper(word))
+    return line
+
+
+# LIST COMPREHENSION FTW!!!
+wordLines = [capitalizeLine(line) for line in wordLines]
+
+# print the lines
+print("\n".join(wordLines))
